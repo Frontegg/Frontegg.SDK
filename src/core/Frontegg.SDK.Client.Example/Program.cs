b68f33aab06b6889f74abe8a1b78ac2013960909
guy lud <guy@frontegg.com>
2020-07-12 00:04:20 +0300

    Remove unneeded dependencies

---
+++ src/core/Frontegg.SDK.Client.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frontegg.SDK.Client.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credentials = new FronteggCredentials("", "");
+        }
+    }
+}
