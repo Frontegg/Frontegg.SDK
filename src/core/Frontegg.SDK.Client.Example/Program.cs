c9ca5350085cc81b251021aec76e7aa4d1f276cb
guy lud <guy@frontegg.com>
2020-07-13 10:52:14 +0300

    remove AwaiterConfigure analyzer

--- src/core/Frontegg.SDK.Client.Example/Program.cs
+++ src/core/Frontegg.SDK.Client.Example/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Frontegg.SDK.Client.Example
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var credentials = new FronteggCredentials("", "");
+            
+            var client = new FronteggClient(credentials);
+
+            await client.Audits.SendAudit("", "{ }");
         }
     }
+    
+    
 }
